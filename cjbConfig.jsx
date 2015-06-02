7da1bd5510770136a4d4ac54bd0660924195607d
YJ Yang <chcokr@gmail.com>
2015-06-02 12:11:18 -0400

    No react-hot-loader in prod build (290KB->277KB)

--- cjbConfig.jsx
+++ cjbConfig.jsx
@@ -5,29 +5,31 @@ module.exports.webpackConfigs = {
     entry: './src/index.jsx',
     target: 'web',
     module: {
-      loaders: [{
-        test: /\.(js|jsx)$/,
-        loader: 'react-hot',
-        exclude: [
-          /node_modules/,
+      loaders: [
+        ...(process.argv[2] !== 'wds' ? [] : {
+          test: /\.(js|jsx)$/,
+          loader: 'react-hot',
+          exclude: [
+            /node_modules/,
 
-          // The following two exceptions will disappear once CWB is published
-          // on npm.
-          // Currently, `npm link` is being used to symlink local copies of
-          // chcokr-js-build and chcokr-webapp-build into this repository's
-          // node_modules/.
-          // Because they are symlinked, their actual resolved paths don't
-          // include the word node_modules, and therefore react-hot-loader ends
-          // up being applied to files within chcokr-js-build and
-          // chcokr-webapp-build.
-          // This causes some really funky errors.
-          // So the following two exceptions are necessary as long as
-          // chcokr-js-build and chcokr-webapp-build are being symlinked into
-          // this repository.
-          /js-build/,
-          /webapp-build/
-        ]
-      }]
+            // The following two exceptions will disappear once CWB is published
+            // on npm.
+            // Currently, `npm link` is being used to symlink local copies of
+            // chcokr-js-build and chcokr-webapp-build into this repository's
+            // node_modules/.
+            // Because they are symlinked, their actual resolved paths don't
+            // include the word node_modules, and therefore react-hot-loader
+            // ends up being applied to files within chcokr-js-build and
+            // chcokr-webapp-build.
+            // This causes some really funky errors.
+            // So the following two exceptions are necessary as long as
+            // chcokr-js-build and chcokr-webapp-build are being symlinked into
+            // this repository.
+            /js-build/,
+            /webapp-build/
+          ]
+        })
+      ]
     }
   }
 };
